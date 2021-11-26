0af4e9d1cb7c544640b6b0c7fae8254d6d5cec7b
Czompi <czompi@czompi.hu>
2021-11-26 01:25:58 +0100

    Initial commit

---
+++ SimpleRGBControl/Logger.cs
@@ -0,0 +1,21 @@
+﻿internal class Logger
+{
+    internal static void LogLine(string message, bool showOnConsole = true, bool showOnFile = true) => Log($"{message}{Environment.NewLine}", showOnConsole);
+
+    internal static void Log(string message, bool showOnConsole = true, bool showOnFile = true)
+    {
+        FileStream fs = File.Open($"bac-{DateTime.Now:yyyy'.'MM'.'dd}.log", FileMode.Append, FileAccess.Write, FileShare.Read);
+
+        StreamWriter fw = new(fs);
+
+        var msg = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {new string('>', message.Count(x => x == '>') + 1)} {message}";
+        if (showOnFile) fw.Write(msg);
+        if (showOnConsole) Console.Write(msg);
+        fw.Flush();
+
+        // Cleanup
+        fw.Close();
+        fs.Close();
+    }
+
+}
--- SimpleRGBControl/Program.cs
+++ SimpleRGBControl/Program.cs
@@ -7,27 +7,27 @@ public static class SimpleRGBControl
     public static void Main(string[] _args)
     {
         var args = new ArgumentHadler(_args);
-        if (args.Length > 0) LogLine("Application is in unattended mode! All outputs will be saved in the log file.");
+        if (args.Length > 0) Logger.LogLine("Application is in unattended mode! All outputs will be saved in the log file.");
         while (!stopProcess)
         {
-            LogLine($"Program started");
+            Logger.LogLine($"Program started");
             IAuraSdk2 sdk = (IAuraSdk2)new AuraSdk();
-            LogLine($"SDK casted");
-            LogLine($"Take over control");
+            Logger.LogLine($"SDK casted");
+            Logger.LogLine($"Take over control");
             sdk.SwitchMode();
-            LogLine($"Control taken over");
-            LogLine($"Get all devices");
+            Logger.LogLine($"Control taken over");
+            Logger.LogLine($"Get all devices");
             Devices = sdk.Enumerate(0);
-            LogLine($"Got all devices");
+            Logger.LogLine($"Got all devices");
             string color;
             if (args.Any())
             {
                 color = args["color"];
             } else
             {
-                Log("Please enter the desired color: ", showOnFile: false);
+                Logger.Log("Please enter the desired color: ", showOnFile: false);
                 color = Console.ReadLine();
-                LogLine($"Please enter the desired color: {color}", showOnConsole: false);
+                Logger.LogLine($"Please enter the desired color: {color}", showOnConsole: false);
             }
             if (color.Equals("q", StringComparison.OrdinalIgnoreCase)) stopProcess = true;
             else UpdateColor(color);
@@ -35,30 +35,12 @@ public static class SimpleRGBControl
 
     }
 
-    private static void LogLine(string message, bool showOnConsole = true, bool showOnFile = true) => Log($"{message}{Environment.NewLine}", showOnConsole);
-
-    private static void Log(string message, bool showOnConsole = true, bool showOnFile = true)
-    {
-        FileStream fs = File.Open($"bac-{DateTime.Now:yyyy'.'MM'.'dd}.log", FileMode.Append, FileAccess.Write, FileShare.Read);
-
-        StreamWriter fw = new(fs);
-
-        var msg = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {new string('>', message.Count(x => x == '>') + 1)} {message}";
-        if(showOnFile)fw.Write(msg);
-        if (showOnConsole) Console.Write(msg);
-        fw.Flush();
-
-        // Cleanup
-        fw.Close();
-        fs.Close();
-    }
-
     private static void UpdateColor(string color)
     {
         color = color.ToUpper();
         if (color.Length == 0)
         {
-            LogLine("Invalid color!");
+            Logger.LogLine("Invalid color!");
             return;
         }
         if (color.Length == 3) color = $"FF{color[0]}{color[0]}{color[1]}{color[1]}{color[2]}{color[2]}";
@@ -72,20 +54,19 @@ public static class SimpleRGBControl
         }
         catch (Exception)
         {
-            LogLine("Invalid color!");
+            Logger.LogLine("Invalid color!");
             return;
         }
         foreach (IAuraSyncDevice dev in Devices)
         {
-            LogLine($"{dev.Name} found");
+            Logger.LogLine($"{dev.Name} found");
             foreach (IAuraRgbLight light in dev.Lights)
             {
-                //Console.WriteLine($"{dev.Name} > AuraRgbLight(Name=\"{light.Name}\",Color=\"{light.Color}\",Red=\"{light.Red}\",Green=\"{light.Green}\",Blue=\"{light.Blue}\") found");
                 light.Color = fixedColor;
             }
-            LogLine($"{dev.Name} > Apply changes");
+            Logger.LogLine($"{dev.Name} > Apply changes");
             dev.Apply();
-            LogLine($"{dev.Name} > Changes applied");
+            Logger.LogLine($"{dev.Name} > Changes applied");
         }
     }
 }
